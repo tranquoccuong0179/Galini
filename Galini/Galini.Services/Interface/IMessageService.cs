fa1dea2c9c14d87f51213f40e0bd24345dee50ca
ffsasa <ngocan2003krp@gmail.com>
2025-01-23 18:11:03 +0700

    Update inteface v3

--- Galini/Galini.Services/Interface/IMessageService.cs
+++ Galini/Galini.Services/Interface/IMessageService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Galini.Models.Request.ListenerInfo;
+using Galini.Models.Response;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,14 @@ using System.Threading.Tasks;
 
 namespace Galini.Services.Interface
 {
-    internal interface IMessageService
+    public interface IMessageService
     {
+        public Task<BaseResponse> CreateLMessage(CreateListenerInfoRequest request, Guid directChatId);
+        public Task<BaseResponse> GetAllMessage(int page, int size);
+        public Task<BaseResponse> GetMessageById(Guid messageId);
+        public Task<BaseResponse> GetMessageByDirectChatId(Guid directChatId);
+        public Task<BaseResponse> SearchMessageByDirectChatId(Guid directChatId, string keyWord);
+        public Task<BaseResponse> UpdateMessage(Guid messageId, CreateListenerInfoRequest request);
+        public Task<BaseResponse> RemoveMessage(Guid messageId);
     }
 }
--- Galini/Galini.Services/Interface/IReviewService.cs
+++ Galini/Galini.Services/Interface/IReviewService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Galini.Models.Request.Review;
+using Galini.Models.Response;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,12 @@ using System.Threading.Tasks;
 
 namespace Galini.Services.Interface
 {
-    internal interface IReviewService
+    public interface IReviewService
     {
+        public Task<BaseResponse> CreateReview(CreateReviewRequest request, Guid bookingId);
+        public Task<BaseResponse> GetAllReview(int page, int size);
+        public Task<BaseResponse> GetReviewById(Guid reviewId);
+        public Task<BaseResponse> UpdateReview(Guid reviewId, CreateReviewRequest request);
+        public Task<BaseResponse> RemoveReview(Guid reviewId);
     }
 }
