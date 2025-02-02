7dce357ae20ee93d4b66060008afe65d4d464f25
tranquoccuong0179 <tranquoccuong0179@gmail.com>
2025-02-02 12:54:10 +0700

    complete topic

---
+++ Galini/Galini.Models/Mapper/TopicMapper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Galini.Models.Entity;
+using Galini.Models.Enum;
+using Galini.Models.Payload.Request.Topic;
+using Galini.Models.Payload.Response.Topic;
+using Galini.Utils;
+
+namespace Galini.Models.Mapper
+{
+    public class TopicMapper : Profile
+    {
+        public TopicMapper()
+        {
+            CreateMap<CreateTopicRequest, Topic>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => Guid.NewGuid()))
+            .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => true))
+            .ForMember(dest => dest.CreateAt, opt => opt.MapFrom(src => TimeUtil.GetCurrentSEATime()))
+            .ForMember(dest => dest.UpdateAt, opt => opt.MapFrom(src => TimeUtil.GetCurrentSEATime()));
+
+            CreateMap<Topic, CreateTopicResponse>();
+
+            CreateMap<Topic, GetTopicResponse>();
+
+            CreateMap<UpdateTopicRequest, Topic>()
+                .ForMember(dest => dest.Name, opt => opt.Condition(src => src.Name != null))
+                .ForMember(dest => dest.UpdateAt, opt => opt.MapFrom(src => TimeUtil.GetCurrentSEATime()));
+        }
+    }
+}
