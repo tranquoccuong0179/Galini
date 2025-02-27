fee17efdbaa0785b27b04f01fac9d73e57618b71
quoccuong <tranquoccuong0179@gmail.com>
2025-02-27 14:28:08 +0700

    Get Transaction

---
+++ Galini/Galini.API/Controllers/TransactionController.cs
@@ -0,0 +1,58 @@
+﻿
+using Galini.API.Constants;
+using Galini.Models.Payload.Response;
+using Galini.Services.Implement;
+using Galini.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Galini.API.Controllers
+{
+    public class TransactionController : BaseController<TransactionController>
+    {
+        private readonly ITransactionService _transactionService;
+        public TransactionController(ILogger<TransactionController> logger, ITransactionService transactionService) : base(logger)
+        {
+            _transactionService = transactionService;
+        }
+
+        [HttpGet(ApiEndPointConstant.Transaction.GetTransactions)]
+        [ProducesResponseType(typeof(BaseResponse), StatusCodes.Status200OK)]
+        [ProducesErrorResponseType(typeof(ProblemDetails))]
+        public async Task<IActionResult> GetTransactions([FromQuery] int? page, [FromQuery] int? size)
+        {
+            int pageNumber = page ?? 1;
+            int pageSize = size ?? 10;
+            var response = await _transactionService.GetTransactions(pageNumber, pageSize);
+
+            return StatusCode(int.Parse(response.status), response);
+        }
+        
+        [HttpGet(ApiEndPointConstant.Transaction.GetTransactionsForAdmin)]
+        [ProducesResponseType(typeof(BaseResponse), StatusCodes.Status200OK)]
+        [ProducesErrorResponseType(typeof(ProblemDetails))]
+        public async Task<IActionResult> GetTransactionsForAdmin([FromQuery] int? page, 
+                                                                 [FromQuery] int? size,
+                                                                 [FromQuery] string? name,
+                                                                 [FromQuery] string? email,
+                                                                 [FromQuery] string? phone,
+                                                                 [FromQuery] bool? sortByPrice)
+        {
+            int pageNumber = page ?? 1;
+            int pageSize = size ?? 10;
+            var response = await _transactionService.GetAllTransaction(pageNumber, pageSize, name, email, phone, sortByPrice);
+
+            return StatusCode(int.Parse(response.status), response);
+        }
+
+        [HttpGet(ApiEndPointConstant.Transaction.GetTransactionById)]
+        [ProducesResponseType(typeof(BaseResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(BaseResponse), StatusCodes.Status404NotFound)]
+        [ProducesErrorResponseType(typeof(ProblemDetails))]
+        public async Task<IActionResult> GetTransactionById([FromRoute] Guid id)
+        {
+            var response = await _transactionService.GetTransaction(id);
+
+            return StatusCode(int.Parse(response.status), response);
+        }
+    }
+}
---
+++ Galini/Galini.Models/Payload/Response/Transaction/GetTransactionAdminResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Galini.Models.Payload.Response.Account;
+
+namespace Galini.Models.Payload.Response.Transaction
+{
+    public class GetTransactionAdminResponse
+    {
+        public decimal? Amount { get; set; }
+        public long? OrderCode { get; set; }
+        public string? Status { get; set; }
+        public string? Type { get; set; }
+        public GetAccountResponse? GetAccountResponse { get; set; }
+    }
+}
