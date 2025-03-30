6ed4374057067217bf20280738b61cea4d2aee29
tranquoccuong0179 <tranquoccuong0179@gmail.com>
2025-03-30 18:52:15 +0700

    update chat but not done

--- Galini/Galini.Services/Interface/IChatClient.cs
+++ Galini/Galini.Services/Interface/IChatClient.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Galini.Models.Entity;
 
 namespace Galini.Services.Interface
 {
     public interface IChatClient
     {
         Task ReceiveMessage(string message);
+        Task ReceiveMessageThread(IEnumerable<Message> messages);
         Task ReceiveMessage(string sender, string message);
         Task ErrorMessage(string error);
         Task OnlineUsers(IEnumerable<string> users);
